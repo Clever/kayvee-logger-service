8b937341f1894c6e2c2e3372a819045bb8d11b0d
Kofi <kofi.ohene-adu@clever.com>
2016-03-01 14:44:13 -0800

    Generate server code.

---
+++ restapi/configure_kayvee_logger_service.go
@@ -0,0 +1,46 @@
+package restapi
+
+import (
+	"net/http"
+
+	errors "github.com/go-swagger/go-swagger/errors"
+	httpkit "github.com/go-swagger/go-swagger/httpkit"
+	middleware "github.com/go-swagger/go-swagger/httpkit/middleware"
+
+	"github.com/Clever/kayvee-logger-service/restapi/operations"
+)
+
+// This file is safe to edit. Once it exists it will not be overwritten
+
+func configureAPI(api *operations.KayveeLoggerServiceAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	api.JSONConsumer = httpkit.JSONConsumer()
+
+	api.JSONProducer = httpkit.JSONProducer()
+
+	api.LogHandler = operations.LogHandlerFunc(func(params operations.LogParams) middleware.Responder {
+		return middleware.NotImplemented("operation .Log has not yet been implemented")
+	})
+	api.PingHandler = operations.PingHandlerFunc(func() middleware.Responder {
+		return middleware.NotImplemented("operation .Ping has not yet been implemented")
+	})
+
+	api.ServerShutdown = func() {}
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
